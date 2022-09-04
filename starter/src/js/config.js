19b5686fdaabe2e52327dd9a4728d41fa4c576d9
HollosT <hollos.tamas@ravensoft.hu>
2022-09-04 07:49:21 +0200

    update

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e3381947-4f4d-471c-a93e-4569d6daad88';
+export const MODEL_CLOSE_SECONDS = 1;
